81dc6bb66c9ffea14ee47005b78f3de157af7dad
MechaXAI <adrianxalzamora@gmail.com>
2022-09-12 09:37:00 -0500

    Implemented message list and animations Create a list of msg Animate messages while expanding Modify UI

--- app/src/main/java/com/example/jetpack_intro/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_intro/MainActivity.kt
@@ -4,14 +4,19 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -30,9 +35,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
-                    // Greeting("Android")
-                    // MessageCard(name = "Kotlin")
+                    // MessageCard(Message("Android", "Jetpack Compose"))
+                    Conversation(SampleData.conversationSample)
+                // Greeting("Android")
+                // MessageCard(name = "Kotlin")
                 }
             }
 
@@ -63,7 +69,19 @@ fun MessageCard(msg: Message) {
         )
         // Add a horizontal space btw the image and the column
         Spacer(modifier = Modifier.width(9.dp))
-        Column {
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        // We toggle the isExpanded variable when we click on this Column
+
+        Column (modifier = Modifier.clickable { isExpanded=!isExpanded }){
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -71,14 +89,46 @@ fun MessageCard(msg: Message) {
             )
             // Add a vertical space btw the author and msg texts
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body,
-                modifier=Modifier.padding(all = 4.dp),
-                style = MaterialTheme.typography.body2
 
-            )
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+
+                )
+            }
         }
     }
 
+}
+
+@Composable
+fun Conversation(messages: List<Message>){
+    LazyColumn{items(messages) { message ->
+        MessageCard(message)
+    }
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    JetPack_introTheme {
+        Conversation(SampleData.conversationSample)
+    }
 }
 
 @Preview(name = "Light Mode")
